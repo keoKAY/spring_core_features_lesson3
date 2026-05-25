7a9eccaa62c43f7bccea3be0d7a977b31d31a289
keoKAY <keokay888@gmail.com>
2026-05-25 17:38:06 +0700

    features: getting values from application

--- src/main/java/co/istad/springcorefeatures/DemoTestController.java
+++ src/main/java/co/istad/springcorefeatures/DemoTestController.java
@@ -1,16 +1,35 @@
 package co.istad.springcorefeatures;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api")
 public class DemoTestController {
+
+    // Get the values from the application.yaml file
+    // for dynamic configuration
+
+    @Value("${app-info.name}")
+    private String appName;
+    @Value("${app-info.domain}")
+    private String domain;
+
+
     @GetMapping("/hello")
-    public String hello() {
-        return "hello";
+    public Map<String,Object> hello() {
+        HashMap<String,Object> map = new HashMap<>();
+        map.put("app-info.name", appName);
+        map.put("domain", domain);
+        return map;
     }
+
+    // Check if you use old thread or new thread
     @GetMapping("/thread")
     public String currentThread()  {
         var thread = Thread.currentThread();
